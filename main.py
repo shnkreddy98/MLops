d7a8ea6a2ae24a09510a53a6c2bb6af52eda7720
Shashank Reddy <reddy@Shashanks-MacBook-Air.local>
2023-07-19 23:40:12 -0700

    stage_01_data_ingestion done

---
+++ main.py
@@ -0,0 +1,14 @@
+from MLops import logger
+from MLops.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
